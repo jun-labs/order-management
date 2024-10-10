432a5ad86ba8b778409b869ccc1414a5e219a313
devjun10 <>
2024-10-10 12:53:15 +0900

    feat: 공통 코드 정의 - CodeAndMessage로 상태 코드, 메시지 관리

---
+++ src/main/java/project/io/app/common/codeandmessage/CommonCodeAndMessage.java
@@ -0,0 +1,33 @@
+package project.io.app.common.codeandmessage;
+
+public enum CommonCodeAndMessage implements CodeAndMessage {
+    OK(200, "OK"),
+    INVALID_ARGUMENT(400, "올바른 파라미터를 입력해주세요."),
+    BAD_REQUEST(400, "올바른 파라미터를 입력해주세요"),
+    INVALID_DATA(400, "데이터가 올바른 양식이 아닙니다."),
+    EMPTY_DATA(400, "외부 데이터를 받아오는데 실패했습니다."),
+    INVALID_URL(404, "올바르지 않은 URL 입니다."),
+    INTERNAL_SERVER(500, "서버 내부 오류입니다."),
+    BAD_GATEWAY(502, "외부 서버와 통신하는 과정에서 에러가 발생했습니다.");
+
+    private final int code;
+    private final String message;
+
+    CommonCodeAndMessage(
+        final int code,
+        final String message
+    ) {
+        this.code = code;
+        this.message = message;
+    }
+
+    @Override
+    public int getCode() {
+        return code;
+    }
+
+    @Override
+    public String getMessage() {
+        return message;
+    }
+}
